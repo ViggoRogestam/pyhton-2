946cb3c0b6caf9421f60eeb89b5fc9c6b8036130
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-10-16 15:06:47 +0200

    app.py

---
+++ Lektion 2/application/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+anteckning_1 = "gå till läkaren"
+app = Flask(__name__)
+
+@app.route('/')
+def route():
+    return f"""
+<!DOCTYPE html>
+<html>
+    <body style="background-color:grey;">
+
+    <h1 style="color:white;"> Viggos anteckningslista</h1>
+    <p style="color:green;"> - städa.</p>
+    <p style="color:white;"> - laga mat.</p>
+    <p style="color:yellow;"> - träna.</p>
+    <p> - {anteckning_1}</p>
+    
+    </body>
+</html> 
+"""
