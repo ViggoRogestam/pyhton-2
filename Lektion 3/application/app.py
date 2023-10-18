7461f6663d5e2b23bfcde7a58349c3fe9c0c76cf
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-10-18 12:59:06 +0200

    flask

--- Lektion 3/application/app.py
+++ Lektion 3/application/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title = "FORZA BAJEN")
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
