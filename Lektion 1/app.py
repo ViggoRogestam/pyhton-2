e1cfc274ec42758fe27d2cd9d25232efecbc943f
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-10-19 09:48:38 +0200

    commit

--- Lektion 1/app.py
+++ Lektion 1/app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__, template_folder='templates')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
 app.run(debug=True)
--- övningsuppgifter/flask_full_course/application/app.py
+++ övningsuppgifter/flask_full_course/application/app.py
@@ -2,14 +2,16 @@ from flask import Flask, jsonify, request
 import pandas as pd
 
 
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def show_lust():
     dict = {
-    "landskap": ["Östergötland", "Östergötland", "Västergötland", "Södermanland", "Södermandland", "Norrbotten", "Gästrikland", "Ångermanland", "Ångermanland", "Ångermandland"],
-    "landsdel": ["Götaland", "Götaland", "Götaland", "Svealand", "Svealand", "Norrland", "Norrland", "Norrland", "Norrland", "Norrland"],
-    "stad": ["Linköping", "Motala", "Mjölby", "Mariefred", "Nyköping", "Piteå", "Sandviken", "Sollefteå", "Kramfors", "Önrsköldsvik"],
-}
+        "landskap": ["Östergötland", "Östergötland", "Västergötland", "Södermanland", "Södermandland", "Norrbotten", "Gästrikland", "Ångermanland", "Ångermanland", "Ångermandland"],
+        "landsdel": ["Götaland", "Götaland", "Götaland", "Svealand", "Svealand", "Norrland", "Norrland", "Norrland", "Norrland", "Norrland"],
+        "stad": ["Linköping", "Motala", "Mjölby", "Mariefred", "Nyköping", "Piteå", "Sandviken", "Sollefteå", "Kramfors", "Önrsköldsvik"],
+    }
+
+
 df = pd.from_dict(dict)
