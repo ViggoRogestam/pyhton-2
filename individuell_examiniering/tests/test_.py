5d02b45b82379faccd5e1e5ada8cf7e2c5685e81
Viggo Rogestam <viggo.rogestam@protonmail.com>
2023-11-08 15:38:17 +0100

    final commit

--- individuell_examiniering/tests/test_.py
+++ individuell_examiniering/tests/test_.py
@@ -27,7 +27,7 @@ def test_Is_online_form():
 
 def test_Confirm_HTTPError_on_api_GET():
     """
-    testar att get request till endpoint '/api' ger en HTTPError
+    testar att en get request till endpoint '/api' ger en HTTPError
     """
     with pytest.raises(urllib.request.HTTPError):
         urllib.request.urlopen("http://127.0.0.1:5000/api",
@@ -39,6 +39,7 @@ def test_api_return_correct_data():
     Detta testar om en av de nestlade dictionaries i listan som returneras från API:et finns in api svaret vilket betyder att svaret är korrekt
     då det innehåller datumet som skickades med i anropet.
     """
+    # skapar en dictionary med det förväntade svaret
     expected_data = {
         "SEK_per_kWh": 0.02218,
         "EUR_per_kWh": 0.002,
@@ -46,22 +47,23 @@ def test_api_return_correct_data():
         "time_start": "2022-12-31T00:00:00+01:00",
         "time_end": "2022-12-31T01:00:00+01:00"
     }
-
+    # kopplar upp mot API:et
     api_response = urllib.request.urlopen(
         "https://www.elprisetjustnu.se/api/v1/prices/2022/12-31_SE3.json")
+    # läser in svaret från API:et
     api_data_bytes = api_response.read()
-    api_data_str = api_data_bytes.decode('utf-8')  # Convert bytes to string
-    # Parse JSON data into a dictionary
+    # Gör om svaret från API:et till en sträng
+    api_data_str = api_data_bytes.decode('utf-8')
+    # Gör om svaret från API:et till en lista av dictionaries
     response_api_data = json.loads(api_data_str)
-
-    # Now, you need to find the correct way to compare expected_data with response_api_data.
-    # This depends on how the API formats its response.
-    # If it returns an array of objects and you need to check if one of those objects matches expected_data:
+    # kontrollerar om expected_data finns i svaret från API:et
     assert expected_data in response_api_data
 
 
 def no_error_on_404():
     client = app.test_client()
     response = client.get('/sida_som_inte_finns')
+    # kollar om statuskoden är 200 (ok) 
     assert response.status_code == 200
+    # kollar om texten 'Sidan du söker finns inte' finns i svaret
     assert 'Sidan du söker finns inte'.encode('utf-8') in response.data
